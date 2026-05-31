8df56bdc18f6c4cf726b379c215e21646be33fc8
CamilaGallegos <camilagallegos340@gmail.com>
2026-05-30 21:10:24 -0300

    creacion de la api rest profesor

--- fei2026tp/backend/aulas/urls.py
+++ fei2026tp/backend/aulas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import carreraMixin
+from .views import carreraMixin, profesorMixinDetail
 
 urlpatterns = [
     path('carreras/', carreraMixin.as_view(), name='carrera-list-create'),
+    path('profesores/<int:pk>/', profesorMixinDetail.as_view(), name='profesor-detail'),
 ]
--- fei2026tp/backend/aulas/views.py
+++ fei2026tp/backend/aulas/views.py
@@ -1,7 +1,7 @@
 from rest_framework import mixins
 from rest_framework import generics
-from .models import carrera
-from .serializers import carreraSerializer
+from .models import carrera, profesor
+from .serializers import carreraSerializer, profesorSerializer
 
 # api rest crear y listar carreras
 class carreraMixin(mixins.ListModelMixin,
@@ -19,3 +19,28 @@ class carreraMixin(mixins.ListModelMixin,
     # peticiones POST (crear)
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
+    
+# api rest detalle, actualizar y eliminar profesor
+class profesorMixinDetail(mixins.RetrieveModelMixin,
+                          mixins.UpdateModelMixin,
+                          mixins.DestroyModelMixin,
+                          generics.GenericAPIView):
+    
+    queryset = profesor.objects.all()
+    
+    serializer_class = profesorSerializer
+
+    # peticiones GET (detalle)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    # peticiones PUT (completo) o PATCH (algun campo) para actualizar
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    # peticiones DELETE (eliminar)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
